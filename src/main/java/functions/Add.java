adfcd816008e34a5e9cae851b385f4cd83964c44
Mariano <marianolinares92@gmail.com>
2014-04-19 12:28:21 -0300

    Initial commit

---
+++ src/main/java/functions/Add.java
@@ -0,0 +1,30 @@
+package functions;
+
+import values.Function;
+import values.Int;
+
+public class Add extends Function {
+
+	@Override
+	public Function eval(Function x) {
+		return new Function(x) {
+		
+			@Override
+			public Function eval(Function y) {
+				return new Int(this.getValue().getLiteral() + y.getLiteral());
+			}
+		};
+		//return new Adds(x);
+	}
+	
+	public class Adds extends Function {
+		public Adds(Function x) {
+			super(x);
+		}
+		
+		@Override
+		public Function eval(Function y) {
+			return new Int(this.getValue().getLiteral() + y.getLiteral());
+		}
+	}
+}
